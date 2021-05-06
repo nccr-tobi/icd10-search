363ea2b69d5df7a393531887d1d2237a0e12f624
Tobias Thüring <tobias.thuering@usb.ch>
2021-05-06 11:45:34 +0200

    chore: add netlify deployment configuration

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('@sveltejs/kit').Config} */
+const adapter = require('@sveltejs/adapter-netlify');
+
 module.exports = {
 	kit: {
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
