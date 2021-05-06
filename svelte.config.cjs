84972a6ae8997104dca48af98ad9a5ccd310598c
Tobias Thüring <tobias.thuering@usb.ch>
2021-05-06 12:10:17 +0200

    chore: add static deployment configuration

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,6 +1,11 @@
 /** @type {import('@sveltejs/kit').Config} */
+const adapter = require('@sveltejs/adapter-static');
+
 module.exports = {
 	kit: {
+		adapter: adapter({
+			fallback: '200.html'
+		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
