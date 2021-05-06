9673681b680d7783d2ccba09e4f23615082c648e
Tobias Thüring <tobias.thuering@usb.ch>
2021-05-06 12:08:21 +0200

    Revert "chore: add netlify deployment configuration"
    This reverts commit 363ea2b69d5df7a393531887d1d2237a0e12f624.

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,9 +1,6 @@
 /** @type {import('@sveltejs/kit').Config} */
-const adapter = require('@sveltejs/adapter-netlify');
-
 module.exports = {
 	kit: {
-		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
