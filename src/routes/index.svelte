a245226734107bd7aa5b9d059ae9e9e65c1e2d3f
Tobias Thüring <tobias.thuering@usb.ch>
2021-05-06 15:03:49 +0200

    fix: prerender index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Search <a href="search">here</a></p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
